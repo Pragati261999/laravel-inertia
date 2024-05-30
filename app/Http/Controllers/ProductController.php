c0f8b153ee36814206afaa4bf82640c18467f17c
CurePe <pragati.synchsofthq@gmail.com>
2024-05-30 17:50:26 +0530

    view issue solved

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,26 +9,26 @@ use Inertia\Inertia;
 class ProductController extends Controller
 {
     // //
-    // public function filter(Request $request)
-    // {
-    //     $query = Products::query();
+    public function filter(Request $request)
+    {
+        $query = Products::query();
 
-    //     if ($request->has('size_id')) {
-    //         $query->whereHas('setItems', function ($q) use ($request) {
-    //             $q->where('size_id', $request->size_id);
-    //         });
-    //     }
+        if ($request->has('size_id')) {
+            $query->whereHas('setItems', function ($q) use ($request) {
+                $q->where('size_id', $request->size_id);
+            });
+        }
 
-    //     if ($request->has('color_id')) {
-    //         $query->whereHas('setItems', function ($q) use ($request) {
-    //             $q->where('color_id', $request->color_id);
-    //         });
-    //     }
+        if ($request->has('color_id')) {
+            $query->whereHas('setItems', function ($q) use ($request) {
+                $q->where('color_id', $request->color_id);
+            });
+        }
 
-    //     $products = $query->get();
+        $products = $query->get();
 
-    //     return response()->json($products);
-    // }
+        return response()->json($products);
+    }
     public function index(Request $request)
     {
         $query = Products::query();
@@ -47,7 +47,7 @@ class ProductController extends Controller
 
         $products = $query->with('setItems.color', 'setItems.size')->get();
 
-        return Inertia::render('Products/Index', [
+        return Inertia::render('Product/Index', [
             'products' => $products,
             'sizes' => \App\Models\Size::all(),
             'colors' => \App\Models\Colors::all(),
